33f82899bdbcf38ac3f768137409f442aeddf0f5
Mithun James <drmithunjames@gmail.com>
2017-05-10 15:40:04 +0530

    asar for Windows and fixes

--- main-process/common/files.js
+++ main-process/common/files.js
@@ -2,52 +2,45 @@ const path = require('path')
 
 exports.getAdbPath = () => {
   if (process.platform === 'linux') {
-    return this.replaceSpaces(path.resolve(__dirname, '../../files/adbkit/linux/adb'))
+    return path.resolve(__dirname, '../../files/adbkit/linux/adb')
   } else if (process.platform === 'darwin') {
-    return this.replaceSpaces(path.resolve(__dirname, '../../files/adbkit/osx/adb'))
+    return path.resolve(__dirname, '../../files/adbkit/osx/adb')
   } else if (process.platform === 'win32') {
-    return this.replaceSpaces(path.resolve(__dirname, '../../files/adbkit/windows/adb.exe'))
+    return path.resolve(__dirname, '../../files/adbkit/windows/adb.exe')
   }
 }
 
 exports.getFastbootPath = () => {
   if (process.platform === 'linux') {
-    return this.replaceSpaces(path.resolve(__dirname, '../../files/adbkit/linux/fastboot'))
+    return path.resolve(__dirname, '../../files/adbkit/linux/fastboot')
   } else if (process.platform === 'darwin') {
-    return this.replaceSpaces(path.resolve(__dirname, '../../files/adbkit/osx/fastboot'))
+    return path.resolve(__dirname, '../../files/adbkit/osx/fastboot')
   } else if (process.platform === 'win32') {
-    return this.replaceSpaces(path.resolve(__dirname, '../../files/adbkit/windows/fastboot.exe'))
+    return path.resolve(__dirname, '../../files/adbkit/windows/fastboot.exe')
   }
 }
 
 exports.get32BitDriver = () => {
-  return this.replaceSpaces(path.resolve(__dirname, '../../files/Motorola_End_User_Driver_Installation_6.3.0_32bit.msi'))
+  return path.resolve(__dirname, '../../files/Motorola_End_User_Driver_Installation_6.3.0_32bit.msi')
 }
 
 exports.get64BitDriver = () => {
-  return this.replaceSpaces(path.resolve(__dirname, '../../files/Motorola_End_User_Driver_Installation_6.3.0_64bit.msi'))
+  return path.resolve(__dirname, '../../files/Motorola_End_User_Driver_Installation_6.3.0_64bit.msi')
 }
 
 exports.getTWRP = () => {
-  return this.replaceSpaces(path.resolve(__dirname, '../../files/twrp-3.1.0-0-potter.img'))
+  return path.resolve(__dirname, '../../files/twrp-3.1.0-0-potter.img')
 }
 
 exports.getBootImage = () => {
-  return this.replaceSpaces(path.resolve(__dirname, '../../files/potter_boot_test7.img'))
+  return path.resolve(__dirname, '../../files/potter_boot_test7.img')
 }
 
 exports.getWlanCustom = () => {
-  return this.replaceSpaces(path.resolve(__dirname, '../../files/wlan_custom.zip'))
+  return path.resolve(__dirname, '../../files/wlan_custom.zip')
 }
 
 exports.getSuperSU = () => {
-  return this.replaceSpaces(path.resolve(__dirname, '../../files/SR3-SuperSU-v2.79-SR3-20170114223742.zip'))
+  return path.resolve(__dirname, '../../files/SR3-SuperSU-v2.79-SR3-20170114223742.zip')
 }
 
-exports.replaceSpaces = (path) => {
-  if (process.platform === 'linux' || process.platform === 'darwin') {
-    return path
-  } else if (process.platform === 'win32') {
-    return '"' + path + '"'
-  }
-}
--- main-process/device/install-drivers.js
+++ main-process/device/install-drivers.js
@@ -1,11 +1,21 @@
 const ipc = require('electron').ipcMain
-const child = require('child_process').execFile
+const child = require('child_process').exec
 const files = require('../common/files')
+const isRunningInAsar = require('electron-is-running-in-asar')
 
-ipc.on('install-32bit-driver', function (event, arg) {
-  child(files.get32BitDriver())
+ipc.on('install-32bit-driver', () => {
+  let file = files.get32BitDriver()
+  this.executeFile(file)
 })
 
-ipc.on('install-64bit-driver', function (event, arg) {
-  child(files.get64BitDriver())
+ipc.on('install-64bit-driver', () => {
+  let file = files.get64BitDriver()
+  this.executeFile(file)
 })
+
+exports.executeFile = (file) => {
+  if (isRunningInAsar()) {
+    file = file.replace('app.asar', 'app.asar.unpacked')
+  }
+  child('"' + file + '"')
+}
